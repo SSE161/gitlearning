c710f6f10fae6f4c478d9a4f3aa0cb9dd18be81f
SSE161 <alanovg@yandex.ru>
2023-09-26 20:33:59 +0300

    Добавлен модуль стринг, удален текст

--- first/nums.py
+++ first/nums.py
@@ -2,13 +2,16 @@ def plus(a, b):
     return a+b
 
 
-text = "newtext"
+
 
 
 def minus(a, b):
     return a-b
 
+def mul(a, b):
+    return a*b
 
 if __name__ == "__main__":
     print(plus(2, 5))
     print(minus(10, 5))
+    print(mul(3, 3))
